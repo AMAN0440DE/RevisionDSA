6d032f20c1ebe945d38676e2103b59a2519780b5
AMAN0440DE <kumar02aman2004@gmail.com>
2025-08-01 09:00:05 +0530

    prime done

--- reverse.java
+++ reverse.java
@@ -18,12 +18,33 @@ public class reverse {
         }
         System.out.print(rev);
     }
+    public static void prime(int a){
+        if(a==2){
+            System.out.println("Prime");
+        }
+        else{
+            boolean isPrime = true;
+            for(int i =2; i <= Math.sqrt(a); i++){
+                if(a%i == 0){
+                    isPrime = false;
+                }
+            }
+            if(isPrime == true){
+                System.out.println("Prime");
+            }
+            else{
+                System.out.println("Not Prime");
+            }
+        }
+    }
     public static void main(String args[]){
         Scanner K = new Scanner(System.in);
         int a = K.nextInt();
         rev(a);
         System.out.println();
         rev2(a);
+        System.out.println();
+        prime(a);
         // int arr[] = new int[a];
         // for(int i =0; i<a; i++){
         //     K.nextInt(arr[i]);
