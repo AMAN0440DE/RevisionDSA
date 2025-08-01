6136c5d601efbd6e2e41942319bee7371ac1b86f
AMAN0440DE <kumar02aman2004@gmail.com>
2025-08-01 10:26:11 +0530

    p2 p3 done

--- D1_Pattern.java
+++ D1_Pattern.java
@@ -8,9 +8,29 @@ public class D1_Pattern {
             System.out.println();
         }
     }
+    public static void p2(int a){
+        char ch = 'A';
+        for(int i = 1; i<= a; i++){
+            for(int j = 1; j<=i; j++){
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+    public static void p3(int a){
+        for(int i = 1; i<= a; i++){
+            for(char ch = 'A'; ch < 'A'+i; ch++ ){
+                System.out.print(ch);
+            }
+            System.out.println();
+        }
+    }
     public static void main(String args[]){
         Scanner K = new Scanner(System.in);
         int a = K.nextInt();
         p1(a);
+        p2(a);
+        p3(a);
     }
 }
